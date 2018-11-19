a1aa5847a7eba20992368153f2901aa1f530e05b
cqu2015 <286931986@qq.com>
2018-11-19 17:28:02 +0800

    测试jdbc分片的设置: 分库分片键不要使用主键

--- sharding-jdbc-example/spring-boot-example/spring-boot-data-mybatis-example/src/main/java/io/shardingsphere/example/spring/boot/mybatis/service/impl/DataGenerator.java
+++ sharding-jdbc-example/spring-boot-example/spring-boot-data-mybatis-example/src/main/java/io/shardingsphere/example/spring/boot/mybatis/service/impl/DataGenerator.java
@@ -3,6 +3,7 @@ package io.shardingsphere.example.spring.boot.mybatis.service.impl;
 import io.shardingsphere.example.spring.boot.mybatis.entity.OrderEntity;
 import io.shardingsphere.example.spring.boot.mybatis.entity.OrderItemEntity;
 import io.shardingsphere.example.spring.boot.mybatis.service.IOrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -14,16 +15,15 @@ import java.util.Random;
  * @Created by IntelliJ IDEA 2018.
  * @Date Create at 2018/11/14 10:11
  */
+@Slf4j
 @Component
 public class DataGenerator {
     @Resource
     IOrderService iOrderService;
 
     public void generateData(int nums){
-        Random random = new Random();
         while(nums-- >0){
-            long orderId = random.nextInt(10000);
-
+            long orderId = generateTaskId();
             OrderEntity orderEntity = new OrderEntity();
             OrderItemEntity orderItemEntity = new OrderItemEntity();
 
@@ -33,11 +33,28 @@ public class DataGenerator {
             iOrderService.addOrder(orderEntity);
 
             orderItemEntity.setOrderId(orderId);
-            orderItemEntity.setOrderId(100000L-nums);
+            orderItemEntity.setOrderId(orderId);
             orderItemEntity.setUserId(orderEntity.getUserId());
             iOrderService.addOrderItem(orderItemEntity);
         }
     }
 
+    /**
+     * 生成taskid 系统当前时间毫秒+""+四位随机数
+     *
+     * @return
+     */
+    private long generateTaskId(){
+        try {
+            Long now = System.currentTimeMillis();
+            Random random = new Random();
+            int randNum = random.nextInt(9999)+10000;
+            String randStr = String.valueOf(now)+String.valueOf(randNum);
+            return Long.parseLong(randStr);
+        }catch (Exception e){
+            log.error("生成随机的taskId失败");
+            return System.currentTimeMillis();
+        }
+    }
 
 }
